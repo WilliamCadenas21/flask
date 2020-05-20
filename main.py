f45ececb99881df620535c7370909610f077b122
WilliamCadenas21 <williamcadenas21@gmail.com>
2020-05-20 17:46:42 -0500

    async process in background with thread

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ from flask import request
 import requests
 import json
 import time
+import threading
 
 UrlRFIDtoCompleteTheDiscount = 'http://127.0.0.1:8001/discountBalance'
 
 app = Flask(__name__)
+
+def processToBilling(dataOfTheTag):
+	time.sleep(7)
+
+	#TODO bank process the tag and the plate here and make the response
+	
+
+	request = requests.post(UrlRFIDtoCompleteTheDiscount, json={
+		"msg":"payment accepted", 
+		"plate":dataOfTheTag['tag'],
+		"epc":dataOfTheTag['plate']}
+	)
+	objResponseJson = request.json()
+	print(objResponseJson)
 
 @app.route('/')
 def hello_world():
@@ -18,27 +33,21 @@ def routepost():
 	print(data)
 	return {'msg':'esta ruta está funcionando correctamente'}
 
+
+
+	
+
 @app.route('/billing', methods = ['POST'])
 def billing():
-	try:
-		dictResponse = request.get_json()
-		plate = print(dictResponse['plate'])
-		processToBilling(dictResponse)
-	except:
-		return {'msg':'hubo un problema manejando la repuesta del lado del servidor'}
+
+	#try:
+	dictResponse = request.get_json()
+	plate = dictResponse['plate']
+	thread = threading.Thread(target=processToBilling, args=(dictResponse,))
+	thread.start()
+	print('start thread')
+	#except:.
+	#	return {'msg':'hubo un problema manejando la repuesta del lado del servidor'}
 
 	return {'msg':'El procesos de cobro se ha inicializado para la placa vehicular #'+plate}	
 
-def processToBilling(dataOfTheTag):
-	time.sleep(7)
-	
-
-	#Todo bank process 
-	
-	request = requests.post(UrlRFIDtoCompleteTheDiscount, json={
-        "msg":"payment accepted", 
-        "plate":dataOfTheTag['tag'],
-		"epc":dataOfTheTag['plate']}
-    )
-	objResponseJson = request.json()
-	print(objResponseJson)
